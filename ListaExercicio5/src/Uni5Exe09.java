b4fd37b270f51145c2088b84fecc369eb77470fc
Diogo Soares <diogora.soares@gmail.com>
2026-04-30 23:46:12 -0300

    Feito questão 9

---
+++ ListaExercicio5/src/Uni5Exe09.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class Uni5Exe09 {
+
+    public static void main(String[] args) {
+        int n;
+        String nome;
+        String nome18 = "";
+        int idade;
+        int contadorMaisDeVinte = 0;
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.printf("Informe quantos alunos deseja inserir: ");
+            n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                System.out.printf("Informe o nome do aluno: ");
+                nome = sc.next();
+                System.out.printf("Informe a idade do aluno: ");
+                idade = sc.nextInt();
+
+                if (idade == 18) {
+                    if (nome18.isEmpty()) {
+                        nome18 = nome;
+                    } else {
+                        nome18 += " e " + nome;
+                    }
+                }
+                if (idade > 20) {
+                    contadorMaisDeVinte++;
+                }
+            }
+            System.out.printf("Nomes dos alunos que tem 18 anos: %s%n", nome18);
+            System.out.printf("Quantidade de alunos que tem idade acima de 20 anos: %d", contadorMaisDeVinte);
+        }
+    }
+}
